960554ea7fba337ce39c8f01329964d40c325a40
dmk2014 <darren.m.keane@students.ittralee.ie>
2017-07-09 20:25:20 +0100

    Menu. Add tween when displaying logo. Add start game text. Capture and listen to spacebar key.

--- src/states/menu.state.ts
+++ src/states/menu.state.ts
@@ -13,12 +13,31 @@ export class MenuState extends Phaser.State {
 
     create(): void {
         this.game.stage.backgroundColor = '3498db'
-        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, Assets.Images.Logo.Key)
+
+        this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 50, Assets.Images.Logo.Key)
         this.logo.anchor.setTo(0.5, 0.5)
+        this.logo.scale.setTo(0)
+        this.game.add.tween(this.logo.scale).to({ x: 1, y: 1 }, 1500).start()
+
+        let textStyle: Phaser.PhaserTextStyle = {
+            font: 'Arial',
+            fontSize: 25,
+            fill: '#FFFFFF'
+        }
+        let text: Phaser.Text = this.game.add.text(this.game.world.centerX, this.game.height - 60, 'Press space to play!', textStyle)
+        text.anchor.setTo(0.5)
+
+        this.game.input.keyboard.addKeyCapture(Phaser.KeyCode.SPACEBAR)
+        this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR).onDown.addOnce(this.onSpacebarPress, this)
     }
 
     update(): void {
         this.logo.angle += 1
+    }
+
+    onSpacebarPress(): void {
+        // TODO
+        console.log('Starting the game...')
     }
 
 }
