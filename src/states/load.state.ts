5e731942217baf5a2539be41967c1e3696cf8fe8
dmk2014 <darren.m.keane@students.ittralee.ie>
2017-07-12 22:02:30 +0100

    Add webfontloader dependency. Load example from Google Fonts.

--- src/states/load.state.ts
+++ src/states/load.state.ts
@@ -1,5 +1,6 @@
 import { MenuState } from './menu.state'
 import * as Assets from '../assets'
+import * as WebFont from 'webfontloader'
 
 
 export class LoadState extends Phaser.State {
@@ -9,9 +10,17 @@ export class LoadState extends Phaser.State {
     preload(): void {
         this.game.load.image(Assets.Images.Logo.Key, Assets.Images.Logo.GetPath())
         this.game.load.audio(Assets.Audio.Menu.Key, [Assets.Audio.Menu.Format.MP3(), Assets.Audio.Menu.Format.OGG()])
+
+        // TODO: Handle situation where font loading fails?
+        WebFont.load({
+            google: {
+                families: ['VT323']
+            },
+            active: () => { this.game.time.events.add(Phaser.Timer.SECOND, this.onLoadComplete, this) }
+        })
     }
 
-    create(): void {
+    onLoadComplete(): void {
         this.game.state.start(MenuState.KEY)
     }
 
